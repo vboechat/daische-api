635921eb74d1d4277a21456fbfabe7f559cc0630
Victor Ribeiro Boechat <victor2005contato@gmail.com>
2023-12-17 19:40:38 -0300

    feat: add sign in user validators

--- src/modules/user/dtos/sign-in.user.dto.ts
+++ src/modules/user/dtos/sign-in.user.dto.ts
@@ -1,8 +1,11 @@
-import { IsEmail, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class SignInUserDto {
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
